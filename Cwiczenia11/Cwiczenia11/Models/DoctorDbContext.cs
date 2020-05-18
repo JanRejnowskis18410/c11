9152c03e3c487f1fed9d5c5974e0c84d83495adc
Jan Rejnowski <jan.rejnowski@gmail.com>
2020-05-18 17:04:26 +0200

    Init commit

---
+++ Cwiczenia11/Cwiczenia11/Models/DoctorDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenia11.Models
+{
+    public class DoctorDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+
+        public DoctorDbContext()
+        {
+
+        }
+
+        public DoctorDbContext(DbContextOptions options)
+        : base (options)
+        {
+
+        }
+    }
+}
